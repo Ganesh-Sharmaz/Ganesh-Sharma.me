7394ac013b360cdf548ee69fff7ce76d7715e1af
Ganesh-Sharmaz <shashanklhr@gmail.com>
2025-08-16 21:51:59 +0530

    fix not secure error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // NO basePath or assetPrefix, so assets load from /_next/...
+  output: "export",            // static export suitable for GitHub Pages
+  trailingSlash: true,         // helps GH Pages serve routes
+  images: { unoptimized: true } // if using next/image with static export
 };
 
 export default nextConfig;
